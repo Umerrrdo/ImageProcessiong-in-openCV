1d82a251ec7d8789e401f623675f733016f6e194
Umerrrdo <umehmood.bscs21seecs@seecs.edu.pk>
2023-10-10 10:48:17 +0500

    modified ConstrastStreching

--- Image Equalization & Histogram/contrastStreching.py
+++ Image Equalization & Histogram/contrastStreching.py
@@ -5,16 +5,17 @@ originalImg = cv2.imread('img/wiki.jpg',0)
 #Create an algorithm which applies contrast stretching to an image
 
 def contrastStretching(img):
-    #Create a new image
-    newImg = img.copy()
-    #Find the minimum and maximum pixel values in the image
-    min = img.min()
-    max = img.max()
-    #Apply the formula to each pixel
+    # Create a new image
+    new_img = img.copy()
+    # Find the minimum and maximum pixel values in the image
+    img_min = img.min()
+    img_max = img.max()
+    # Apply the formula to each pixel
     for i in range(0, img.shape[0]):
         for j in range(0, img.shape[1]):
-            newImg[i,j] = (img[i,j] - min) * (255/(max - min))
-    return newImg
+            new_img[i, j] = (img[i, j] - img_min) * (255 / (img_max - img_min))
+            # print(new_img[i, j]," ",img[i, j])
+    return new_img
 
 #Apply the algorithm to the image
 newImg = contrastStretching(originalImg)
