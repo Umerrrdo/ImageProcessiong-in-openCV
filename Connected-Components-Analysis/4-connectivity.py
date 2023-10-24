4b3fe28a8a5b89e8e0787c0064aa3624eb5626bf
Umerrrdo <umehmood.bscs21seecs@seecs.edu.pk>
2023-10-24 13:02:35 +0500

    8 and 4 connectivity modidfied

--- Connected-Components-Analysis/4-connectivity.py
+++ Connected-Components-Analysis/4-connectivity.py
@@ -1,43 +1,61 @@
 import cv2
 import numpy as np
 
+
 img = cv2.imread('sample.jpg')
-grayScale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-_, binaryImage = cv2.threshold(grayScale, 128, 255, cv2.THRESH_BINARY)
-colorImage = np.zeros_like(img)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+_, binaryImage = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY)
+labelEquivalence = {}
+currLabel = 1
 
-# Define 4-connectivity neighbors (top, bottom, left, right)
-neighbors = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-counterLabel = 1
-labelColorDic = {}
+def find_root(label):
+    if label not in labelEquivalence:
+        labelEquivalence[label] = label
+    if label != labelEquivalence[label]:
+        labelEquivalence[label] = find_root(labelEquivalence[label])
+    return labelEquivalence[label]
 
-for i in range(binaryImage.shape[0]):
-    for j in range(binaryImage.shape[1]):
-        if binaryImage[i, j] == 255:
-            connectedLabels = []
 
-            # Check 4-connectivity neighbors for labels
-            for dx, dy in neighbors:
-                x, y = i + dx, j + dy
-                if 0 <= x < binaryImage.shape[0] and 0 <= y < binaryImage.shape[1]:
-                    neighbor_label = colorImage[x, y]
-                    if neighbor_label.any():
-                        connectedLabels.append(neighbor_label)
-
-            # If no connected labels, assign a new label and color
-            if not connectedLabels:
-                uniqueColor = (np.random.randint(0, 256), np.random.randint(0, 256), np.random.randint(0, 256))
-                labelColorDic[counterLabel] = uniqueColor
-                labelColor = uniqueColor
-                counterLabel += 1
+# First pass to label connected components
+for i in range(1, binaryImage.shape[0] - 1):
+    for j in range(1, binaryImage.shape[1] - 1):
+        if binaryImage[i, j] != 0:
+            neighbors = [binaryImage[i, j - 1], binaryImage[i - 1, j], binaryImage[i - 1, j - 1],
+                         binaryImage[i - 1, j + 1]]
+            neighbors = [x for x in neighbors if x != 0]
+            if not neighbors:
+                binaryImage[i, j] = currLabel
+                currLabel += 1
             else:
-                # Assign the pixel the color of the first connected neighbor
-                labelColor = connectedLabels[0]
+                min_neighbor = min(neighbors)
+                binaryImage[i, j] = min_neighbor
+                for neighbor in neighbors:
+                    if neighbor != min_neighbor:
+                        labelEquivalence[neighbor] = min_neighbor
 
-            colorImage[i, j] = labelColor
+for i in range(1, binaryImage.shape[0] - 1):
+    for j in range(1, binaryImage.shape[1] - 1):
+        if binaryImage[i, j] != 0:
+            binaryImage[i, j] = find_root(binaryImage[i, j])
+color_dict = {}
 
+coloredImage = np.zeros((binaryImage.shape[0], binaryImage.shape[1], 3), dtype=np.uint8)
+
+for i in range(1, binaryImage.shape[0] - 1):
+    for j in range(1, binaryImage.shape[1] - 1):
+        label = binaryImage[i, j]
+        if label != 0 and label not in color_dict:
+            color_dict[label] = np.random.randint(0, 256, 3)  # Generate random colors
+
+for i in range(1, binaryImage.shape[0] - 1):
+    for j in range(1, binaryImage.shape[1] - 1):
+        label = binaryImage[i, j]
+        if label != 0:
+            coloredImage[i, j] = color_dict[label]
+
+# Display the labeled img and the colored result
 cv2.imshow("Original Image", img)
-cv2.imshow("Labeled Image", colorImage)
+cv2.imshow("Colored Result", coloredImage)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- Connected-Components-Analysis/8-connectivity.py
+++ Connected-Components-Analysis/8-connectivity.py
@@ -1,42 +1,63 @@
 import cv2
 import numpy as np
 
+
 img = cv2.imread('sample.jpg')
-grayScale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-_, binaryImage = cv2.threshold(grayScale, 128, 255, cv2.THRESH_BINARY)
-coloredImage = np.zeros_like(img)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+_, binImage = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY)
+labelEquivalence = {}
+currLabel = 1
 
 # Define 8-connectivity neighbors
-neighbors = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-counterLabel = 1
-labelColorDic = {}
+neighbors = [(-1, -1), (-1, 0), (-1, 1), (0, -1)]
 
-for i in range(binaryImage.shape[0]):
-    for j in range(binaryImage.shape[1]):
-        if binaryImage[i, j] == 255:
-            connectedLabels = []
 
-            # Check 8-connectivity neighbors for labels
-            for dx, dy in neighbors:
-                x, y = i + dx, j + dy
-                if 0 <= x < binaryImage.shape[0] and 0 <= y < binaryImage.shape[1]:
-                    neighbor_label = coloredImage[x, y]
-                    if neighbor_label.any():
-                        connectedLabels.append(neighbor_label)
+def find_root(label):
+    if label not in labelEquivalence:
+        labelEquivalence[label] = label
+    if label != labelEquivalence[label]:
+        labelEquivalence[label] = find_root(labelEquivalence[label])
+    return labelEquivalence[label]
 
-            # If no connected labels, assign a new label and color
-            if not connectedLabels:
-                uniqueColor = (np.random.randint(0, 256), np.random.randint(0, 256), np.random.randint(0, 256))
-                labelColorDic[counterLabel] = uniqueColor
-                labeled_color = uniqueColor
-                counterLabel += 1
+
+# First pass to label connected components with 8-connectivity
+for i in range(1, binImage.shape[0] - 1):
+    for j in range(1, binImage.shape[1] - 1):
+        if binImage[i, j] != 0:
+            neighbors_labels = [binImage[i + dx, j + dy] for dx, dy in neighbors]
+            neighbors_labels = [x for x in neighbors_labels if x != 0]
+            if not neighbors_labels:
+                binImage[i, j] = currLabel
+                currLabel += 1
             else:
-                # Assign the pixel the color of the first connected neighbor
-                labeled_color = connectedLabels[0]
+                min_neighbor = min(neighbors_labels)
+                binImage[i, j] = min_neighbor
+                for neighbor in neighbors_labels:
+                    if neighbor != min_neighbor:
+                        labelEquivalence[neighbor] = min_neighbor
 
-            coloredImage[i, j] = labeled_color
+for i in range(1, binImage.shape[0] - 1):
+    for j in range(1, binImage.shape[1] - 1):
+        if binImage[i, j] != 0:
+            binImage[i, j] = find_root(binImage[i, j])
+colorDic = {}
 
+coloredImage = np.zeros((binImage.shape[0], binImage.shape[1], 3), dtype=np.uint8)
+
+for i in range(1, binImage.shape[0] - 1):
+    for j in range(1, binImage.shape[1] - 1):
+        label = binImage[i, j]
+        if label != 0 and label not in colorDic:
+            colorDic[label] = np.random.randint(0, 256, 3)  # Generate random colors
+
+for i in range(1, binImage.shape[0] - 1):
+    for j in range(1, binImage.shape[1] - 1):
+        label = binImage[i, j]
+        if label != 0:
+            coloredImage[i, j] = colorDic[label]
+
+# Display the original img and the colored result
 cv2.imshow("Original Image", img)
-cv2.imshow("Labeled Image", coloredImage)
+cv2.imshow("Colored Result", coloredImage)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
